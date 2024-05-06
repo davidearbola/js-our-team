88dd49e08b7179d17dcff0086223cfdfaf2e3ca7
davidearbola <davidearbola@gmail.com>
2024-05-06 16:31:56 +0200

    M2-stampo in come stringa  con e senza funzione

--- js/function.js
+++ js/function.js
@@ -1,1 +1,5 @@
-console.log(`function`);
+function stampa(oggetto) {
+	let elemento = document.createElement(`p`);
+	elemento.innerHTML = `${oggetto.nome}, ${oggetto.ruolo}, ${oggetto.immagine}`;
+	document.getElementById(`prova`).append(elemento);
+}
--- js/main.js
+++ js/main.js
@@ -33,9 +33,28 @@ let team = [
 	},
 ];
 
+// M1
 // ciclo for per iterare all'interno dell'array e stampare solo due proprietà degli oggetti
+// for (let i = 0; i < team.length; i++) {
+// 	const member = team[i];
+// 	console.log(member);
+// 	console.log(
+// 		`Membro del team: ${member.nome}, ${member.ruolo}, ${member.immagine}`
+// 	);
+// }
+
+// M2 senza function
+// stampo in pagina nome, ruolo e immagine solo come una stringa senza funzione
+// for (let i = 0; i < team.length; i++) {
+// 	const member = team[i];
+// 	let elemento = document.createElement(`h2`);
+// 	elemento.innerText = `Membro del team: ${member.nome}, ${member.ruolo}, ${member.immagine}`;
+// 	document.getElementById(`prova`).append(elemento);
+// }
+
+// M2 con function
+// stampo in pagina nome, ruolo e immagine solo come una stringa CON funzione
 for (let i = 0; i < team.length; i++) {
 	const member = team[i];
-	console.log(member);
-	console.log(`Membro del team: ${member.nome}, ${member.ruolo}`);
+	stampa(member);
 }
